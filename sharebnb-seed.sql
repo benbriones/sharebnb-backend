f0ec9b62e41b20f35940bda9da8d09f65e6f4bb2
Ben Briones <benbriones50@gmail.com>
2024-04-16 14:55:57 -0700

    added tables

---
+++ sharebnb-seed.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (username, password, first_name, last_name, email, phone, is_admin)
+VALUES
+    ('testUser1', 'password1', 'first1', 'last1', 'e@e.com', '8187172020', FALSE),
+    ('testUser2', 'password2', 'first2', 'last2', 'e@e.com', '8187172020', FALSE),
+    ('adminUser', 'password3', 'first3', 'last3', 'e@e.com', '8187172020', TRUE);
+
+
+INSERT INTO properties (title, address, description, price, owner)
+VALUES
+    ('awesome oasis', '123 awesome street, indio', 'awesome1', 250, 'testUser1'),
+    ('bad oasis', '123 bad street, indio', 'bad1', 300, 'testUser2'),
+    ('worse oasis', '123 worse street, indio', 'worse1', 350, 'adminUser');
+
+-- INSERT INTO images (key, caption, source, property_id)
